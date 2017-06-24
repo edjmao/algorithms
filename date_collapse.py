1f556c8e10de945f795d26c3c725aa4b185d6a48
Edward Mao <ed@edspace.com>
2017-06-23 22:36:09 -0500

    Add coalesce algorithm.

--- date_collapse.py
+++ date_collapse.py
@@ -3,12 +3,23 @@
 def coalesce_dates(dates):
     starts = [ date[0] for date in dates ]
     ends = [ date[1] for date in dates ]
-
+    starts.sort()
+    ends.sort()
+    
     count = 0
 
     coalesced = []
     
     while starts:
-        start = starts.pop
+        start = starts.pop(0)
         count += 1
         
+        while count > 0:
+            if starts[0] <= ends[0]:
+                starts.pop(0)
+                count += 1
+            else:
+                end = ends.pop(0)
+                count -= 1
+            
+        coalesced.append((start,end))

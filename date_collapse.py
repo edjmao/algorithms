099edeca72e0dd09c459f96644a1a46d8cf9baf6
Edward Mao <ed@edspace.com>
2017-06-23 23:21:07 -0500

    Fixed a bug; added doctests.

--- date_collapse.py
+++ date_collapse.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
+import doctest
+
 def coalesce_dates(dates):
+    """
+    Coalesces all date pairs into combined date pairs that makes it easy to find free time gaps.
+
+    >>> from date_collapse import coalesce_dates
+    >>> dates = [(1,4),(2,8),(12,16),(16,21)]
+    >>> cdates = coalesce_dates(dates)
+    >>> print(cdates)
+    [(1, 21), (12, 16)]
+    """
+
     starts = [ date[0] for date in dates ]
     ends = [ date[1] for date in dates ]
     starts.sort()
@@ -14,12 +26,16 @@ def coalesce_dates(dates):
         start = starts.pop(0)
         count += 1
         
-        while count > 0:
+        while count > 0 and starts:
             if starts[0] <= ends[0]:
                 starts.pop(0)
                 count += 1
             else:
                 end = ends.pop(0)
                 count -= 1
-            
+        end = ends.pop()
         coalesced.append((start,end))
+    return coalesced
+
+if __name__=="__main__":
+    doctest.testmod()
